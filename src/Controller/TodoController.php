5e8a8f2f2775e295863188ffa1fe0b8e01afe8d8
thib92 <thibhenry92@gmail.com>
2019-06-08 17:35:00 +0200

    Add form

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Todo;
+use App\Entity\User;
+use App\Form\TodoType;
 use App\Repository\TodoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends AbstractController
@@ -12,13 +15,29 @@ class TodoController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(TodoRepository $todoRepository)
+    public function index(Request $request, TodoRepository $todoRepository)
     {
+        /** @var User $user */
+        $user = $this->getUser();
 
-        $todos = $todoRepository->findByUser($this->getUser());
+        $todos = $todoRepository->findByUser($user);
+        $todo = new Todo();
+        $todo->setUser($user);
+        $todo->setDone(false);
+        $form = $this->createForm(TodoType::class, $todo);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+            return $this->redirectToRoute('index');
+        }
 
         return $this->render('todo/index.html.twig', [
             'todos' => $todos,
+            'todoForm' => $form->createView()
         ]);
     }
 
@@ -26,12 +45,15 @@ class TodoController extends AbstractController
      * @Route("/todo/delete/{id}", name="delete")
      */
     public function delete(Todo $todo) {
-        if ($todo->getUser() !== $this->getUser()) {
+        /** @var User $user */
+        $user = $this->getUser();
+        if ($todo->getUser()->getId() !== $user->getId()) {
             return $this->redirectToRoute('index');
         }
 
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->remove($todo);
+        $entityManager->flush();
         return $this->redirectToRoute('index');
     }
 }
